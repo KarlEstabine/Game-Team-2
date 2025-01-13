f7be0f4d633581c38b311e2e75873e63c27bbd23
Kenneth Schuetz <rane@mstsage.com>
2025-01-12 20:13:47 -0500

    Added the button functions to work.

--- Team Project/Assets/Scripts/buttonFunctions.cs
+++ Team Project/Assets/Scripts/buttonFunctions.cs
@@ -1,16 +1,30 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class buttonFunctions : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void Resume()
     {
-        
+        // Unpause the game via resume button
+        gameManager.instance.StateUnpause();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Restart()
     {
-        
+        // Restart the level via restart button
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        // Unpause the game
+        gameManager.instance.StateUnpause();
+    }
+
+    public void Quit()
+    {
+    #if UNITY_EDITOR
+        // If in Unity Editor, stop playing the game
+        UnityEditor.EditorApplication.isPlaying = false;
+    #else
+        // If in a build, quit the application
+        Application.Quit();
+    #endif
     }
 }
